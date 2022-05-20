27d9c95ccc016374ec7acc58eb1dd4996e9cb722
surekha091 <69725949+surekha091@users.noreply.github.com>
2022-05-20 13:17:02 +0530

    Update index.jsx

--- src/modules/home/index.jsx
+++ src/modules/home/index.jsx
@@ -4,5 +4,8 @@ import logo from './logo.png';
 export default () => (
   <Fragment>
     <img src={logo} alt="logo" />
+    <h1>
+        Welcome to 3D-Pester React-App
+    </h1>
   </Fragment>
 );
